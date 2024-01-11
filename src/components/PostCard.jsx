88dab3ab78feecba1f8fdb54f496cbfa7969eb77
ramalt <ramazanalltuntepe@gmail.com>
2024-01-11 19:00:47 +0300

    Post card link fix

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -13,7 +13,7 @@ import { red } from "@mui/material/colors";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import { Checkbox, Stack } from "@mui/material";
+import { Box, Checkbox, Stack } from "@mui/material";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
 const ExpandMore = styled((props) => {
@@ -37,7 +37,7 @@ export default function PostCard() {
   };
 
   return (
-    <Card component={"a"} href="#postid" sx={{ textDecoration: "none" , margin : '5px'}}>
+    <Card sx={{ textDecoration: "none", margin: "5px" }}>
       <CardHeader
         avatar={
           <Avatar
@@ -53,78 +53,84 @@ export default function PostCard() {
         }
         title="Shrimp and Chorizo Paella"
         subheader="january 11, 2024"
+        component={"a"}
+        href="#profile"
+        sx={{ textDecoration: "none" , color:"text.primary"}}
       />
-      <CardContent>
-        <Typography variant="h6" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
-      </CardContent>
-      <Stack direction={"row"} gap={1}>
-        <CardMedia
-          component="img"
-          height="194"
-          image="https://i.pinimg.com/550x/e7/55/09/e75509d701d000df3c2accb5816d2a02.jpg"
-          alt="Paella dish"
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image="https://i.pinimg.com/236x/bf/e6/86/bfe68610b2a537611b214434ac6ba86e.jpg"
-          alt="Paella dish"
-        />
-      </Stack>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <Checkbox
-            icon={<FavoriteBorder />}
-            checkedIcon={<Favorite color="error" />}
-          />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ChatBubbleOutlineIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <Box component={"a"} href="#postid" sx={{ textDecoration: "none" }}>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
+          <Typography variant="h6" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
           </Typography>
         </CardContent>
-      </Collapse>
+        <Stack direction={"row"} gap={1}>
+          <CardMedia
+            component="img"
+            height="194"
+            image="https://i.pinimg.com/550x/e7/55/09/e75509d701d000df3c2accb5816d2a02.jpg"
+            alt="Paella dish"
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image="https://i.pinimg.com/236x/bf/e6/86/bfe68610b2a537611b214434ac6ba86e.jpg"
+            alt="Paella dish"
+          />
+        </Stack>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite color="error" />}
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ChatBubbleOutlineIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+              stirring occasionally until lightly browned, 6 to 8 minutes.
+              Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic,
+              tomatoes, onion, salt and pepper, and cook, stirring often until
+              thickened and fragrant, about 10 minutes. Add saffron broth and
+              remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes
+              and peppers, and cook without stirring, until most of the liquid
+              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+              reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is
+              just tender, 5 to 7 minutes more. (Discard any mussels that
+              don&apos;t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Box>
     </Card>
   );
 }
